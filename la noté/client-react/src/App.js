b1b97f9a88a4bf79346570880c58c3211adba82a
Troy Medina <56012563+troyamedina@users.noreply.github.com>
2020-01-12 20:04:17 -0800

    Add files via upload

---
+++ la noté/client-react/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Project from "./components/Project";
+import "./App.css";
+
+// Form reference
+const form = {}
+form.noteText = document.querySelector('#formNoteText');
+form.addButton = document.querySelector('#formAddButton');
+form.color = document.querySelector('#formColor');
+
+const notes = document.querySelector('#notes');
+
+form.noteText.focus();
+
+// Functions
+function addNote() {
+  let text = form.noteText.value;
+  let note = document.createElement('div');
+  let deleteButton = document.createElement('span');
+
+  note.classList.add('note');
+  note.classList.add(form.color.value);
+  note.innerHTML = `<div class='note-text'>${text}</div>`;
+  deleteButton.classList.add('note-delete');
+  deleteButton.innerHTML = '&times;';
+
+  note.appendChild(deleteButton);  
+  notes.appendChild(note);
+
+  form.noteText.value = '';
+  form.noteText.focus();
+
+  addListenerDeleteButton(deleteButton);
+}
+
+function addListenerDeleteButton(deleteButton) {
+  deleteButton.addEventListener('click', function (e) {
+    e.stopPropagation();      
+    deleteNote(e);
+  });
+}
+
+function deleteNote(e) {
+  let eventNote = e.target.parentNode;
+  eventNote.parentNode.removeChild(eventNote);
+}
+
+
+
+// Event Listeners
+form.addButton.addEventListener('click', function (e) {
+  e.preventDefault();  
+  if (form.noteText.value != '') {
+    addNote();
+  }
+})
+function App() {
+  return (
+    <div className="App">
+      <Project />
+    </div>
+  );
+}
+
+export default App;
